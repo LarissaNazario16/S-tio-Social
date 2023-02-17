8f90f98c5259721c4bf2486186c5a8a0f02efdf4
LarissaNazario16 <114767462+LarissaNazario16@users.noreply.github.com>
2023-02-17 17:01:45 -0300

    Validação da senha

---
+++ js/senha.js
@@ -0,0 +1,24 @@
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Senhas diferentes!");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+function Toggle() {
+    var temp = document.getElementById("password");
+    if (temp.type === "password") {
+        temp.type = "text";
+    }
+    else {
+        temp.type = "password";
+    }
+}
+
